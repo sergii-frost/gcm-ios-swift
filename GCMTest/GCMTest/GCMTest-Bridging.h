619db2144db702f550327b249a3f8c29f4be8847
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-02-29 15:31:07 +0100

    Basic setup of GCM

---
+++ GCMTest/GCMTest/GCMTest-Bridging.h
@@ -0,0 +1,14 @@
+//
+//  GCMTest-Bridging.h
+//  GCMTest
+//
+//  Created by Sergii Nezdolii on 29/02/16.
+//  Copyright © 2016 FrostDigital. All rights reserved.
+//
+
+#ifndef GCMTest_Bridging_h
+#define GCMTest_Bridging_h
+
+#import <Google/CloudMessaging.h>
+
+#endif /* GCMTest_Bridging_h */
