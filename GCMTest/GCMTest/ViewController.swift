b3c767ca7d011563f3292f2631bfbed1b4cf2713
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-02-29 14:49:48 +0100

    Initial project setup

---
+++ GCMTest/GCMTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCMTest
+//
+//  Created by Sergii Nezdolii on 29/02/16.
+//  Copyright © 2016 FrostDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
